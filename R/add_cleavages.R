c3aa4a5e9b27d5ed774d19fceb5d7bf29f511b78
Charlie Bayne <baynec2@gmail.com>
2024-04-12 17:30:25 -0700

    fix

--- R/add_cleavages.R
+++ R/add_cleavages.R
@@ -17,12 +17,12 @@ add_cleavages = function(joined_with_library){
 
 
   # Combining nterm and cterm
-  cleavages = bind_cols(nterm,cterm[,2:3])
+  cleavages = dplyr::bind_cols(nterm,cterm[,2:3])
 
 
   # Building final data frame.
   output = dplyr::bind_cols(joined_with_library,cleavages) %>%
-    dplyr::select(library_reference_id,library_real_sequence,Peptide,nterm,nterm_cleavage_pos,cterm,cterm_cleavage_pos,dplyr::everything())
+    dplyr::select(library_reference_id,library_real_sequence,Peptide,nterm,nterm_cleavage_pos,cterm,cterm_cleavage_pos,dplyr::everything(),-peptide)
 
 
 
