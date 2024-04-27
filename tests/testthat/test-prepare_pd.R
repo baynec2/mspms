1c6debd07ecc3d52f1edaba2246a542a9a40854e
Charlie Bayne <baynec2@gmail.com>
2024-04-26 18:42:25 -0700

    fixed test file paths for automated testing

--- tests/testthat/test-prepare_pd.R
+++ tests/testthat/test-prepare_pd.R
@@ -1,6 +1,6 @@
 test_that("there are no duplicate peptides after processing", {
   expect_true({
-    dat = mspms::prepare_pd("../testdata/proteome_discovere_output.xlsx")
+    dat = mspms::prepare_pd("../testdata/proteome_discoverer_output.xlsx")
 
     dat %>%
       dplyr::pull(Peptide) %>%
--- tests/testthat/test-prepare_peaks.R
+++ tests/testthat/test-prepare_peaks.R
@@ -1,6 +1,6 @@
 test_that("there are no duplicate peptides after processing", {
   expect_true({
-      dat = mspms::prepare_peaks("..//testdata/protein-peptides-lfq_2.csv",
+      dat = mspms::prepare_peaks("../testdata/protein-peptides-lfq_2.csv",
                                  "../testdata/protein-peptides-id_2.csv")
       dat %>%
       dplyr::pull(Peptide) %>%
