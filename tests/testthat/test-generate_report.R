d6da83adb6f980b088e582a7f2a139597e10376e
Charlie Bayne <baynec2@gmail.com>
2024-09-25 21:14:32 -0700

    removed generate report test

--- tests/testthat/test-generate_report.R
+++
@@ -1,9 +0,0 @@
-test_that("generate report does not generate an error", {
-  expect_no_error(
-    generate_report(
-      mspms::peaks_prepared_data,
-      mspms::peptide_library,
-      n = 4
-    )
-  )
-})
